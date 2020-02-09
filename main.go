4245f4e18c5123cc0d53dda35b39fa1bdc8828db
jun <juncwork@gmail.com>
2020-02-09 13:47:29 +0800

    add worker and itemsaver

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"crawler/engine"
+	"crawler/persist"
 	"crawler/schduler"
 	"crawler/zhenai/parser"
 )
 
 func main() {
+	itemChan, err := persist.ItemSaver("dating_profile")
+	if err != nil {
+		panic(err)
+	}
 	e := engine.ConcurrentEngine{
 		//Scheduler: &schduler.SimpleScheduler{},
 		Scheduler:   &schduler.QueuedScheduler{},
 		WorkerCount: 100,
+		ItemChan:    itemChan,
 	}
 
 	//e.Run(engine.Request{
