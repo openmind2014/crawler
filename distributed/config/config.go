9aaab5e4fd631efd25c0a59dd3fd75e4a985198d
jun <juncwks@gmail.com>
2020-02-10 15:58:21 +0800

    Distributed version

--- distributed/config/config.go
+++ distributed/config/config.go
@@ -7,11 +7,13 @@ const (
 	ParseProfile  = "ParseProfile"
 	NilParser     = "NilParser"
 
-	ItemSaverPort = 1234
-	WorkerPort0   = 9000
-
+	// ElasticSearch
 	ElasticIndex = "dating_profile"
 
+	// RPC Endpoints
 	ItemSaverRpc    = "ItemSaverService.Save"
 	CrawlServiceRpc = "CrawlService.Process"
+
+	// Rate limiting
+	Qps = 50
 )
--- distributed/main.go
+++ distributed/main.go
@@ -3,23 +3,32 @@ package main
 import (
 	"crawler/distributed/config"
 	"crawler/distributed/persist/cilent"
+	"crawler/distributed/rpcsupport"
 	"crawler/distributed/worker/client"
 	"crawler/engine"
 	"crawler/schduler"
 	"crawler/zhenai/parser"
-	"fmt"
+	"flag"
+	"log"
+	"net/rpc"
+	"strings"
+)
+
+var (
+	itemSaverHost = flag.String("itemsaver_host", "", "itemsaver host")
+	workerHosts   = flag.String("worker_hosts", "", "worker host (comma separated)")
 )
 
 func main() {
-	itemChan, err := cilent.ItemSaver(fmt.Sprintf(":%d", config.ItemSaverPort))
+	flag.Parse()
+	itemChan, err := cilent.ItemSaver(*itemSaverHost)
 	if err != nil {
 		panic(err)
 	}
 
-	processor, err := client.CreateProcessor()
-	if err != nil {
-		panic(err)
-	}
+	pool := createClientPool(strings.Split(*workerHosts, ","))
+	processor := client.CreateProcessor(pool)
+
 	e := engine.ConcurrentEngine{
 		Scheduler:        &schduler.QueuedScheduler{},
 		WorkerCount:      100,
@@ -36,4 +45,27 @@ func main() {
 		Url:    "https://www.zhenai.com/zhenghun/shanghai",
 		Parser: engine.NewFuncParser(parser.ParseCity, config.ParseCity),
 	})
+}
+
+func createClientPool(hosts []string) chan *rpc.Client {
+	var clients []*rpc.Client
+	for _, h := range hosts {
+		client, err := rpcsupport.NewClient(h)
+		if err == nil {
+			clients = append(clients, client)
+			log.Printf("Connected to %s", h)
+		} else {
+			log.Printf("Error connecting to %s: %v", h, err)
+		}
+	}
+
+	out := make(chan *rpc.Client)
+	go func() {
+		for {
+			for _, client := range clients {
+				out <- client
+			}
+		}
+	}()
+	return out
 }
